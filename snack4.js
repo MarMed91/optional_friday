4056682ca619822935034edf62b268683eff910f
MarkoM91 <markomedic91@gmail.com>
2019-03-25 00:37:03 +0100

    commit8

---
+++ snack4.js
@@ -0,0 +1,47 @@
+function getSum(arr) {
+
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    console.log(arr[i]);
+  }
+  return sum;
+}
+
+function ajaxArray() {
+
+  var quantity = Number(prompt("Quanti elementi vuoi includere nell'array"));
+
+    $.ajax({
+
+    url:"https://www.boolean.careers/api/array/integers",
+    data: {min:1, max:100, items: quantity},
+    method: "GET",
+    success: function(data, state) {
+
+      if (data.success) {
+
+         var sum = getSum(data.response);
+          console.log(sum);
+      }
+    },
+    error: function(request, state, error) {
+
+      console.log("request", request);
+      console.log("state", state);
+      console.log("error", error);
+    }
+
+   });
+
+
+ }
+
+
+
+function init() {
+
+ajaxArray();
+}
+
+$(document).ready(init);
